f0ed177a2cd4fb7846db8d8750e3c75b5cca07c5
Kristof Dreier <kontakt@kristofdreier.de>
2016-12-12 17:42:00 +0100

    added state slide

--- src/components/BigCode.js
+++ src/components/BigCode.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const BigCode = styled.code`
     background-color: transparent;
-    font-size: 3.3vw;
+    font-size: ${props => props.size ? props.size : '3.3vw'};
     font-family: monospace;
     font-weight: bold;
     color: ${props => props.fade ? 'rgba(0, 36, 82, .3)' : '#002452'};
@@ -11,7 +11,8 @@ const BigCode = styled.code`
     margin-left: ${props => props.indent ? `${props.indent * 3}vw` : 0};
     text-align: ${props => props.align ? props.align : 'center'};
     transition: color .125s ease-in-out;
-    white-space: nowrap;
+    white-space: ${props => props.multiline ? 'pre' : 'nowrap'};
+    width: ${props => props.width ? props.width : 'initial'};
 `;
 
 export default BigCode;
